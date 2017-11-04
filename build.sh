1e2900f7dddab15f1418c4607de3453b9bfab875
Rodrigo Silva <rbsilva.ti@gmail.com>
2017-11-04 10:45:02 +0100

    Using tensorflow gpu base image and running with nvidia-docker

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t tensorflow-jupyter:0.0.1-gpu docker
 docker rm tensorflow-jupyter
-docker run -p 8888:8888 -it -v $(pwd)/notebooks:/notebooks --name=tensorflow-jupyter tensorflow-jupyter:0.0.1-gpu
+nvidia-docker run -p 8888:8888 --rm -it -v $(pwd)/notebooks:/notebooks --name=tensorflow-jupyter tensorflow-jupyter:0.0.1-gpu
