0b3ff3ab8e7824e3376aee335aeae9aada44b357
Rodrigo Silva <rbsilva.ti@gmail.com>
2018-10-02 12:17:51 -0300

    Using nvidia-docker and updates in notebooks

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t tensorflow-jupyter:0.0.1-gpu docker
 docker rm tensorflow-jupyter
-nvidia-docker run -p 8888:8888 --rm -it -v $(pwd)/notebooks:/notebooks --name=tensorflow-jupyter tensorflow-jupyter:0.0.1-gpu
+optirun nvidia-docker run -p 8888:8888 --rm -it -v $(pwd)/notebooks:/notebooks --name=tensorflow-jupyter tensorflow-jupyter:0.0.1-gpu
